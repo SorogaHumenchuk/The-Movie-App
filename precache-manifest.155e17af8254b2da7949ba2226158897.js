91a1795212c0e7187f2fc5f9c65a06cb9840f7d6
Seroga Humenchuk <niva1999niva@gmail.com>
2019-04-18 04:59:29 +0300

    Updates

---
+++ precache-manifest.155e17af8254b2da7949ba2226158897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5cb075a87b20c3a79cec8af589eade4",
+    "url": "/The-Movie-App/static/media/logo.b5cb075a.svg"
+  },
+  {
+    "revision": "110bfc4978f6288b43bb",
+    "url": "/The-Movie-App/static/js/runtime~main.223db8a7.js"
+  },
+  {
+    "revision": "426ab9fc8a2488237b54",
+    "url": "/The-Movie-App/static/js/main.9b133961.chunk.js"
+  },
+  {
+    "revision": "c35f4e00c8a2fbe6de4c",
+    "url": "/The-Movie-App/static/js/2.686b5346.chunk.js"
+  },
+  {
+    "revision": "426ab9fc8a2488237b54",
+    "url": "/The-Movie-App/static/css/main.86e1653d.chunk.css"
+  },
+  {
+    "revision": "c35f4e00c8a2fbe6de4c",
+    "url": "/The-Movie-App/static/css/2.9d9bebdc.chunk.css"
+  },
+  {
+    "revision": "b571ded1e3a262823ea00919185ba043",
+    "url": "/The-Movie-App/index.html"
+  }
+];
